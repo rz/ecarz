27c90bfb44260a50a71b6d143d071609cabd0fbe
Rodrigo Guzman <rodguze@gmail.com>
2016-07-14 15:49:14 -0400

    object oriented implementation of grid + beginnings of handling 2d case

--- ecarz.py
+++ ecarz.py
@@ -1,4 +1,77 @@
 import argparse
+import collections
+
+
+def handle_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+            '-d', '--dimension', help='The dimension of the automaton. Only 1d and 2d automata are supported',
+            type=int, default=1)
+    parser.add_argument(
+            '-r', '--rule', help='Rule number (according to the Wolfram convention) of the automaton to simulate',
+            type=int, required=True)
+    parser.add_argument(
+            '-g', '--grid-size', help='Number of cells in the grid in 1d. In 2d the grid will be a grid-size x grid-size square.',
+            type=int, default=101)
+    parser.add_argument(
+            '-s', '--steps', help='Number of steps to simulate the automaton for',
+            type=int, default=50)
+    parser.add_argument(
+            '-G', '--grid', help='The initial state of the grid. Only supported in 1d, ignored if --dimension is 2. Specifying this option overrides the grid-size option. By default, an empty cell is represented by a single space character and a filled cell by *, but this can be changed with the --char-zero and --char-one options.',
+            type=str, default=None)
+    parser.add_argument(
+            '--char-zero', help='The character to use for a cell in state 0 in human-friendly representations. Default is a space.',
+            type=str, default=' ')
+    parser.add_argument(
+            '--char-one', help='The character to use for a cell in state 1 in human-friendly representations. Default is *.',
+            type=str, default='*')
+
+    args = parser.parse_args()
+
+    if args.grid is not None and args.dimension == 1:
+        grid_size = len(args.grid)
+        grid_str = args.grid
+    else:
+        grid_size = args.grid_size
+        grid_str = None
+    return args.dimension, args.rule, grid_str, grid_size, args.steps, args.char_zero, args.char_one
+
+
+class Grid(collections.UserList):
+    def __init__(self, dimension, size, char0=' ', char1='*'):
+        self.char0, self.char1 = char0, char1
+        if dimension not in [1, 2]:
+            raise ValueError('Only 1d and 2d grids are supported.')
+
+        self.dimension = dimension
+        self.size = size
+
+        middle_row = [int(c) for c in '1'.center(size, '0')]  # one 1 cell at the middle. hacky, but the center() method is handy
+        if dimension == 1:
+            self.data = middle_row
+        else:
+            self.data = [[0 for _ in range(size)] for _ in range(size - 1)]
+            self.data.insert(size//2, middle_row)
+
+    def __str__(self):
+        if self.dimension == 1:
+            return ''.join(str(i) for i in self.data).replace('0', self.char0).replace('1', self.char1)
+        else:
+            raise NotImplemented
+
+    def get_neighbor_tuples(self):
+        if self.dimension == 1:
+            return zip([0] + grid[:-1], grid, grid[1:] + [0])
+        else:
+            raise NotImplemented
+
+    def set_state(self, state):
+        self.data = state
+
+
+def parse_grid(grid_str, char0, char1):
+    return [int(c) for c in grid_str.replace(char0, '0').replace(char1, '1')]
 
 
 def get_next_cell_state(rule, cells):
@@ -18,65 +91,21 @@ def get_next_grid_state(rule, grid):
     # we use "blank cell" boundary conditions ie always assume there is a blank
     # cell to the left and right of the grid's ends.
     # todo: support other types of boundary conditions?
-    grid_zip = zip([0] + grid[:-1], grid, grid[1:] + [0])
-    new_grid = [get_next_cell_state(rule, (l, c, r)) for l, c, r in grid_zip]
+    cells = grid.get_neighbor_tuples()
+    new_grid = Grid(grid.dimension, grid.size, grid.char0, grid.char1)
+    new_grid.set_state([get_next_cell_state(rule, (l, c, r)) for l, c, r in cells])
     return new_grid
 
 
-def initialize_grid():
-    return [int(c) for c in '1'.center(grid_size, '0')]  # one 1 cell at the middle. hacky, but the center() method is handy
-
-
-def grid_to_string(grid, char0, char1):
-    return ''.join(str(i) for i in grid).replace('0', char0).replace('1', char1)
-
-
-def parse_grid(grid_str, char0, char1):
-    return [int(c) for c in grid_str.replace(char0, '0').replace(char1, '1')]
-
-
-def handle_args():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-            '-r', '--rule', help='Rule number (according to the Wolfram convention) of the automaton to simulate',
-            type=int, required=True)
-    parser.add_argument(
-            '-g', '--grid-size', help='Number of cells in the grid',
-            type=int, default=101)
-    parser.add_argument(
-            '-s', '--steps', help='Number of steps to simulate the automaton for',
-            type=int, default=50)
-    parser.add_argument(
-            '-G', '--grid', help='The initial state of the grid. Specifying this option overrides the grid-size option. By default, an empty cell is represented by a single space character and a filled cell by *, but this can be changed with the --char-zero and --char-one options.',
-            type=str, default=None)
-    parser.add_argument(
-            '--char-zero', help='The character to use for a cell in state 0 in human-friendly representations. Default is a space.',
-            type=str, default=' ')
-    parser.add_argument(
-            '--char-one', help='The character to use for a cell in state 1 in human-friendly representations. Default is *.',
-            type=str, default='*')
-
-    args = parser.parse_args()
-
-    if args.grid is None:
-        grid_size = args.grid_size
-        grid = None
-    else:
-        grid_size = len(args.grid)
-        grid = parse_grid(args.grid, args.char_zero, args.char_one)
-    return args.rule, grid, grid_size, args.steps, args.char_zero, args.char_one
-
-
 if __name__ == '__main__':
-    rule, grid, grid_size, max_steps, char0, char1 = handle_args()
-    if grid is None:
-        grid = initialize_grid()
-
-    print(grid_to_string(grid, char0, char1))  # initial step
+    dimension, rule, grid_str, grid_size, max_steps, char0, char1 = handle_args()
+    grid = Grid(dimension, grid_size, char0=char0, char1=char1)
+    if dimension == 1 and grid_str is not None:
+        grid.set_state(parse_grid(grid_str, char0, char1))
+    print(grid)
 
     # evolve the automaton for the given number of steps and print each one
     for _ in range(max_steps):
         grid = get_next_grid_state(rule, grid)
-        print(grid_to_string(grid, char0, char1))
+        print(grid)
 
